c4d40142b3ce85545b52300566fccdf5bebfc1aa
Pradnya Rane <pradnya.rane11@gmail.com>
2026-06-16 20:14:49 +0530

    Remove database password

--- db.js
+++ db.js
@@ -3,20 +3,11 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "mysql-35fbc717-department-management-system.e.aivencloud.com",
   user: "avnadmin",
-  password: "AVNS_yOJCXtD0nL3iwKsGojx",
+  password: process.env.DB_PASSWORD,
   database: "defaultdb",
   port: 27745,
   ssl: {
     rejectUnauthorized: false
-  }
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Database Connection Failed");
-    console.log(err);
-  } else {
-    console.log("Database Connected Successfully");
   }
 });
 
